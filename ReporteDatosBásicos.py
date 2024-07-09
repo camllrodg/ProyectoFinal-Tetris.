32cf0fc71acdde5c832da9338865e78021e696ca
camllrodg <camntgg@gmail.com>
2024-07-09 17:00:08 -0400

    ReporteMejorado

--- ReporteDatosBásicos.py
+++ ReporteDatosBásicos.py
@@ -23,18 +23,29 @@ def main():
 def FuncReporte(lineaString, archivoJuegos):
   reporte=open("ReporteDatosBasicos", "w")  #Será un archivo txt.
 
+  opcion=input("¿Desea buscar los datos de algún jugador? (si/no): ")
+  if opcion=="si":
+      iden=input("Ingrese el identificador del usuario que desea buscar: ")
+
   reporte.write("                                                                          Jugador                                                                            \n")
-  reporte.write("Código de Juego                            Puntos ganados                    Fecha                      Hora           \n")
+  reporte.write("Código de Juego                           Puntos ganados                    Fecha                      Hora           \n")
   reporte.write("  *******                                    *********                       ******                    *******                   \n")
   
-  listaDatos=lineaString.split("/")
   while lineaString:  #Con ya tener la variable "linea" un valor, ya de por sí es True.
     listaDatos=lineaString.split("/") #para eliminar la barra. Los datos los colocará en una lista.
-    reporte.write(f"             {listaDatos[0]}                         {listaDatos[2]}                                                              {listaDatos[3]}                          {listaDatos[4]}")
-    lineaString=archivoJuegos.readline() #Inicializamos :).
+    valor=0 
+    #Aquí colocamos el condicional 
+    valor=BusquedaSec(listaDatos, iden) 
+    if valor==1: 
+      reporte.write(f" {listaDatos[0]}                         {listaDatos[2]}                       {listaDatos[3]}                        {listaDatos[4]}")  
+    lineaString=archivoJuegos.readline() #Inicializamos :).   
     lineaString=lineaString.decode("utf-8")
 
   reporte.close()
+
+def BusquedaSec(listaDatos, iden):
+  if listaDatos[1]==iden:
+      return 1
 
 if __name__=="__main__":
   main()
